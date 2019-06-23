733c6a8921226d20c741a599b2ac8dd636b44d83
pol <121955907@qq.com>
2019-06-23 12:50:33 +0800

    chore: fix router

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -4,9 +4,13 @@ var router = new Router()
 router
 .get('/login', admin.loginPage)
 .post('/login', admin.login)
+.get('/logout', admin.logout)
 .get('/index', admin.index)
 .get('/blog', admin.blog)
 .get('/article', admin.article)
+.get('/article/:id', admin.article)
+.post('/article', admin.create_article)
+.post('/article/:id', admin.update_article)
 
 module.exports = {
   admin: router
