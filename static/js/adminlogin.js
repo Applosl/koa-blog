9b38641d5621a558267a102781e6111665d3740c
pol <121955907@qq.com>
2019-06-23 12:52:06 +0800

    feat: add article admin-web

--- static/js/adminlogin.js
+++ static/js/adminlogin.js
@@ -1,7 +1,8 @@
-
+// "use strict";
 function login(e) {
   var form = e.target.form;
   var formData = new FormData(form)
+  $(e.target).button('loading');
   $.ajax({
     type: "POST",
     url: "/admin/login",
@@ -10,13 +11,17 @@ function login(e) {
     contentType: false, // 注意这里应设为false
     processData: false,
     success: function(data) {
-
+      if (data.code === 0) {
+          location.href = '/admin/index'
+      } else {
+        alert(data.message);
+      }
     },
     error: function(xhr, textStatus, errorThrown) {
-
+      alert("登录失败");
     },
     complete: function(xhr, textStatus) {
-
+      $(e.target).button('reset');
     }
   });
 }
